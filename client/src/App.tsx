9c64527c7df8910f9f1bdd0293f2526f507432f9
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-11-12 20:50:12 +0800

    debug home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import {
     BrowserRouter as Router,
     Route,
@@ -7,55 +7,89 @@ import {
     Redirect,
 } from "react-router-dom";
 
-import Login from "./components/Login/Login"
-import Register from "./components/Register/Register"
-import Home from "./components/Home/Home"
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
 
-import login from "./services/login"
+import login from "./services/login";
 
 export interface CUserContext {
-    username: string
-    setUsername: React.Dispatch<React.SetStateAction<string>>
-    setUserAuthentication: React.Dispatch<React.SetStateAction<boolean | undefined>>
+    username: string;
+    setUsername: React.Dispatch<React.SetStateAction<string>>;
+    setUserAuthentication: React.Dispatch<
+        React.SetStateAction<boolean | undefined>
+    >;
 }
 
-export const userContext = React.createContext<CUserContext | undefined>(undefined)
+export const userContext = React.createContext<CUserContext | undefined>(
+    undefined
+);
 
 export default function App(): JSX.Element {
-    const [username, setUsername] = useState<string>("")
-    const [userIsAuthenticated, setUserAuthentication] = useState<boolean | undefined>(undefined)
+    const [username, setUsername] = useState<string>("");
+    const [userIsAuthenticated, setUserAuthentication] = useState<
+        boolean | undefined
+    >(undefined);
 
     useEffect(() => {
         async function getlogin() {
-            const { isAuthenticated, data: { username } } = await login.get()
+            const {
+                isAuthenticated,
+                data: { username },
+            } = await login.get();
             if (isAuthenticated) {
-                setUsername(username)
+                setUsername(username);
             }
-            setUserAuthentication(isAuthenticated)
+            setUserAuthentication(isAuthenticated);
         }
-        getlogin()
-    }, [])
+        getlogin();
+    }, []);
 
     return (
         <Router>
-            <userContext.Provider value={{ username, setUsername, setUserAuthentication }}>
+            <userContext.Provider
+                value={{ username, setUsername, setUserAuthentication }}
+            >
                 <div className="App">
-
                     {userIsAuthenticated === undefined ? null : (
                         <Switch>
-                            <Route exact path="/"
-                                component={() => userIsAuthenticated ? <Home /> : <Redirect to="/login" />}
+                            <Route
+                                exact
+                                path="/"
+                                component={() =>
+                                    userIsAuthenticated ? (
+                                        <Home />
+                                    ) : (
+                                        <Redirect to="/login" />
+                                    )
+                                }
                             />
-                            <Route exact path="/login"
-                                component={() => userIsAuthenticated ? <Redirect to="/" /> : <Login />}
+                            <Route
+                                exact
+                                path="/login"
+                                component={() =>
+                                    userIsAuthenticated ? (
+                                        <Redirect to="/" />
+                                    ) : (
+                                        <Login />
+                                    )
+                                }
                             />
-                            <Route exact path="/register"
-                                component={() => userIsAuthenticated ? <Redirect to="/" /> : <Register />}
+                            <Route
+                                exact
+                                path="/register"
+                                component={() =>
+                                    userIsAuthenticated ? (
+                                        <Redirect to="/" />
+                                    ) : (
+                                        <Register />
+                                    )
+                                }
                             />
                         </Switch>
                     )}
                 </div>
             </userContext.Provider>
         </Router>
-    )
+    );
 }
