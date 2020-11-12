93b08519bec5a25c710b34957adb192ed5820f00
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-11-12 20:50:50 +0800

    debug home page for not taking full screen

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,36 +1,53 @@
-import React, { useState } from 'react'
-import { Grid, ThemeProvider, createMuiTheme } from '@material-ui/core'
-import Appbar from './Header/Appbar'
-import Content from './Content/Content'
+import React, { useState } from "react";
+import {
+    Grid,
+    ThemeProvider,
+    createMuiTheme,
+    makeStyles,
+} from "@material-ui/core";
+import Appbar from "./Header/Appbar";
+import Content from "./Content/Content";
+
+const useStyles = makeStyles(() => ({
+    home: {
+        height: "100vh",
+        display: "grid",
+        gridTemplateRows: "min-content 1fr",
+    },
+    content: {
+        height: "100%",
+    },
+}));
 
 export interface CThemeContext {
-    darkTheme: boolean
-    setDarkTheme: React.Dispatch<React.SetStateAction<boolean>>
+    darkTheme: boolean;
+    setDarkTheme: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const themeContext = React.createContext<CThemeContext | undefined>(undefined)
+export const themeContext = React.createContext<CThemeContext | undefined>(
+    undefined
+);
 
 export default function Home(): JSX.Element {
-    const [darkTheme, setDarkTheme] = useState<boolean>(false)
+    const classes = useStyles();
+    const [darkTheme, setDarkTheme] = useState<boolean>(false);
     const theme = createMuiTheme({
         palette: {
-            type: darkTheme ? "dark" : "light"
-        }
-    })
+            type: darkTheme ? "dark" : "light",
+        },
+    });
     return (
         <ThemeProvider theme={theme}>
             <themeContext.Provider value={{ darkTheme, setDarkTheme }}>
-                <div>
-                    <Grid container direction="column">
-                        <Grid item>
-                            <Appbar />
-                        </Grid>
-                        <Grid item>
-                            <Content />
-                        </Grid>
+                <Grid container direction="column" className={classes.home}>
+                    <Grid item>
+                        <Appbar />
                     </Grid>
-                </div>
+                    <Grid item className={classes.content}>
+                        <Content />
+                    </Grid>
+                </Grid>
             </themeContext.Provider>
         </ThemeProvider>
-    )
+    );
 }
